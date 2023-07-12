f3e7a963b8de88e7887958bdfbd153a31ff796c3
SubhasishKabi <99902592+SubhasishKabi@users.noreply.github.com>
2023-07-12 22:28:00 +0530

    Final commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,26 +5,29 @@ import styles from "../styles";
 import { navVariants } from "../utils/motion";
 
 //inset is same as absolute. instead of specifying all top, right, bottom ,left, we can do it in one line in inset
-const Navbar = () => (
-  <motion.nav
-    variants={navVariants}
-    initial="hidden"
-    whileInView="show"
-    className={`${styles.xPaddings} py-8 relative`}
-  >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img
-        src="/search.svg"
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className="font-extrabold text-[24px] leading-[34px] text-white">
-        METAVERSUS
-      </h2>
-      <img src="/menu.svg" alt="menu" className="w-[24px] object-contain" />
-    </div>
-  </motion.nav>
-);
-
+const Navbar = () => {
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className="absolute w-[50%] inset-0 gradient-01" />
+      <div
+        className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+      >
+        <img
+          src="/search.svg"
+          alt="search"
+          className="w-[24px] h-[24px] object-contain"
+        />
+        <h2 className="font-extrabold text-[24px] leading-[34px] text-white">
+          METAVERSUS
+        </h2>
+        <img src="/menu.svg" alt="menu" className="w-[24px] object-contain" />
+      </div>
+    </motion.nav>
+  );
+};
 export default Navbar;
